cae98f8e9585237ddfd9cbbb43878a976f3c7e29
Bayu Adi Prasetiyo <bayu@leson.id>
2020-08-18 01:52:14 +0700

    Initial Commit

---
+++ Hello World/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by Bayu Adi Prasetiyo on 18/08/20.
+//  Copyright © 2020 Bayu Adi Prasetiyo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
